c3d132b99cc6768d20b482d2f97bbdc0c72a7a77
WeeXee <pauline.cabee@ynov.com>
2023-10-02 12:04:56 +0200

    map2

--- map.js
+++ map.js
@@ -1,19 +1,21 @@
-// Import Leaflet CSS and JavaScript
-import 'leaflet/dist/leaflet.css';
-import 'leaflet/dist/leaflet.js';
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 51.505, lng: -0.09 }, // Initial coordinates
+        zoom: 13, // Initial zoom level
+        styles: [
+            // You can customize the map's style here
+            {
+                featureType: 'all',
+                elementType: 'labels.text.fill',
+                stylers: [{ color: '#ffffff' }]
+            }
+        ]
+    });
 
-// Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13); // Set the initial coordinates and zoom level
-
-// Add a tile layer (you can use various map providers, e.g., OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// You can also add markers, polygons, and other map elements here as needed
-// For example, adding a marker
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A marker here.');
-
-// Customize the map's appearance
-map.getContainer().style.background = '#444'; // Set the map's background color
+    // Add a marker to the map
+    var marker = new google.maps.Marker({
+        position: { lat: 51.5, lng: -0.09 },
+        map: map,
+        title: 'A Marker Here'
+    });
+}
