a6cd644a00d53055469f94ee80df905dc570df63
lucas <lucas.escaffre@ynov.com>
2023-10-04 15:31:26 +0200

    continue: server and fix with html css

--- routes/js/map.js
+++ routes/js/map.js
@@ -1,19 +1,24 @@
 // Initialize the map
-var map = L.map('map').setView([51.505, -0.09], 13); // Set the initial coordinates and zoom level
+const map = L.map('map').setView([43.610376, 1.431881], 8); // Set the initial coordinates and zoom level
 
 // Add a tile layer (you can use various map providers, e.g., OpenStreetMap)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+let osm = L.tileLayer ('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo (map);
 
 // You can also add markers, polygons, and other map elements here as needed
 // For example, adding a marker
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A marker here.')
-    .openPopup();
+let marker = L.marker([43.610376, 1.431881]).addTo(map)
+
+let Basemaps = {
+    "OSM": osm
+}
+
+let Overlaysmaps = {
+    "Marker": marker
+}
+
+L.control.layers(Basemaps, Overlaysmaps).addTo(map)
 
 // Customize the map's appearance
 map.getContainer().style.background = '#444'; // Set the map's background color
-
-
-console.log("ok")
--- server.js
+++ server.js
@@ -1,3 +1,44 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
 
-const app = express();
+const app = express()
+const port = process.env.PORT || 3000
+const __dirname = import.meta.url.substring(7, import.meta.url.lastIndexOf("/"))
+
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + "/routes"))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.listen(port, () => {
+    console.log("Server is listening on port 3000")
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/routes/html/index.html")
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + "/routes/html/Register.html")
+});
+
+app.post('/register', (req, res) => {
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password;
+    let confirm = req.body.confirmPassword;
+    console.log("Username: " + username + " Password: " + password + " Email: " + email + " Confirm: " + confirm)
+    res.send("Welcome " + username + " !")
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + "/routes/html/Login.html")
+});
+
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+    console.log("Username: " + username + " Password: " + password)
+    res.send("Welcome " + username + " !")
+});
+app.get('/vitrine', (req, res) => {
+    res.sendFile(__dirname + "/routes/html/Vitrine.html")
+});
